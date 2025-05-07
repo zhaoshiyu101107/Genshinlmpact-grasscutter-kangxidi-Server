edc2c41b2fab46471a963c3883dd9721b597691b
昭诗雨 <zhaodijie@163.com>
2025-05-08 00:55:37 +0800

    Create zong.bat

---
+++ data/bat/zong.bat
@@ -0,0 +1,41 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFAFcTQuGAES0A5EO4f7+02xxRcvcl964pHlcEl+ypQ7sbYAa8GbiU5CSHTLEQZ2kk5dBr4j/ZlJGt3CEC+KEsgLxRk214EomEmZ7iS3ZjyRb
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAFcTQuGAES0A5EO4f7+02xxRcvcl964pHlcEl+ypQ7sbYAa8GbiU5CSHTLEQZ2kk5dBr4j/ZlJGt3CEC8Odpw6vT1CMhg==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+echo 打开MongoDB
+cd %~dp0MongoDB\
+start start.exe
+
+echo 打开grasscutter
+cd %~dp0grasscutter
+start start.exe
+
